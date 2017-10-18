fc69a9691ebac5ea14017f6f3b7c09a739c0539b
David Christiansen <david@davidchristiansen.dk>
2017-10-18 07:56:44 -0400

    Remove more bits of scribble/lp

--- lp/lang/common.rkt
+++ lp/lang/common.rkt
@@ -4,62 +4,16 @@
          module-begin/plain
          module-begin/doc)
 
-(require (for-syntax racket/base syntax/boundmap racket/list
-                     syntax/strip-context syntax/parse))
+(require (for-syntax racket/base syntax/strip-context syntax/parse))
 
 (begin-for-syntax
   (define-syntax-class whitespace-string
     (pattern
      s:str #:when (andmap char-whitespace? (string->list (syntax->datum #'s)))))
-  
-  (define first-id #f)
-  (define main-id #f)
-  (define (mapping-get mapping id)
-    (free-identifier-mapping-get mapping id (lambda () '())))
-  ;; maps a chunk identifier to its collected expressions
-  (define chunks (make-free-identifier-mapping))
-  ;; maps a chunk identifier to all identifiers that are used to define it
-  (define chunk-groups (make-free-identifier-mapping))
-  (define (get-chunk id) (mapping-get chunks id))
-  (define (add-to-chunk! id exprs)
-    (unless first-id (set! first-id id))
-    (when (eq? (syntax-e id) '<*>) (set! main-id id))
-    (free-identifier-mapping-put!
-     chunk-groups id
-     (cons id (mapping-get chunk-groups id)))
-    (free-identifier-mapping-put!
-     chunks id
-     `(,@(mapping-get chunks id) ,@exprs)))
 
   (define module-code (box '()))
   (define (add-to-code! exprs)
     (set-box! module-code (cons exprs (unbox module-code)))))
-
-
-(define-syntax (reconstitute stx)
-  (define orig-stx (syntax-case stx () [(_ orig) #'orig]))
-  (define (restore nstx d) (datum->syntax orig-stx d nstx nstx))
-  (define (shift nstx) (replace-context orig-stx nstx))
-  #;(define body
-    (let ([main-id (or main-id first-id)])
-      (restore
-       main-id
-       (let loop ([block (get-chunk main-id)])
-         (append-map
-          (lambda (expr)
-            (if (identifier? expr)
-                (let ([subs (get-chunk expr)])
-                  (if (pair? subs)
-                      (begin 
-                             (loop subs))
-                      (list (shift expr))))
-                (let ([subs (syntax->list expr)])
-                  (if subs
-                      (list (restore expr (loop subs)))
-                      (list (shift expr))))))
-          block)))))                               
-  (with-syntax ([(body ...) (strip-comments (restore orig-stx (apply append (reverse (unbox module-code)))))])
-    #`(begin body ...)))
 
 
 (define-for-syntax (reconstruct orig-stx)
@@ -67,41 +21,6 @@
   (define (shift nstx) (replace-context orig-stx nstx))
   (strip-comments (restore orig-stx (apply append (reverse (unbox module-code))))))
 
-(define-syntax (tangle stx)
-  (define chunk-mentions '())
-  (unless first-id
-    (raise-syntax-error 'scribble/lp "no chunks"))
-  (define orig-stx (syntax-case stx () [(_ orig) #'orig]))
-  (define (restore nstx d) (datum->syntax orig-stx d nstx nstx))
-  (define (shift nstx) (replace-context orig-stx nstx))
-  (define body
-    (let ([main-id (or main-id first-id)])
-      (restore
-       main-id
-       (let loop ([block (get-chunk main-id)])
-         (append-map
-          (lambda (expr)
-            (if (identifier? expr)
-                (let ([subs (get-chunk expr)])
-                  (if (pair? subs)
-                      (begin (set! chunk-mentions (cons expr chunk-mentions))
-                             (loop subs))
-                      (list (shift expr))))
-                (let ([subs (syntax->list expr)])
-                  (if subs
-                      (list (restore expr (loop subs)))
-                      (list (shift expr))))))
-          block)))))                               
-  (with-syntax ([(body ...) (strip-comments body)]
-                ;; construct arrows manually
-                [((b-use b-id) ...)
-                 (append-map (lambda (m)
-                               (map (lambda (u)
-                                      (list (syntax-local-introduce m) 
-                                            (syntax-local-introduce u)))
-                                    (mapping-get chunk-groups m)))
-                             chunk-mentions)])
-    #`(begin body ... (let ([b-id (void)]) b-use) ...)))
 
 (define-for-syntax (strip-comments body)
   (cond
