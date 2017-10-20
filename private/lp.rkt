cf8447e992b59daaffae8e869142e06c4bf6438c
David Christiansen <david@davidchristiansen.dk>
2017-10-20 14:57:43 -0400

    Make it possible to hide program and PROGRAM blocks

--- private/lp.rkt
+++ private/lp.rkt
@@ -1,44 +1,44 @@
 #lang scheme/base
 
-(require (for-syntax scheme/base syntax/boundmap)
-         scribble/scheme scribble/decode scribble/manual scribble/struct)
+(require (for-syntax racket/base syntax/parse)
+         scribble/scheme scribble/manual scribble/struct)
 
 (begin-for-syntax
-  ;; maps chunk identifiers to a counter, so we can distinguish multiple uses
-  ;; of the same name
-  (define chunk-numbers (make-free-identifier-mapping))
-  (define (get-chunk-number id)
-    (free-identifier-mapping-get chunk-numbers id (lambda () #f)))
-  (define (inc-chunk-number id)
-    (free-identifier-mapping-put! chunk-numbers id (+ 1 (free-identifier-mapping-get chunk-numbers id))))
-  (define (init-chunk-number id)
-    (free-identifier-mapping-put! chunk-numbers id 2)))
+  (define-splicing-syntax-class hide-opt
+    (pattern (~seq #:hide)
+             #:with hide? #t)
+    (pattern (~seq)
+             #:with hide? #f)))
 
-(define-syntax-rule (define-code chunk-id racketblock)
-  (define-syntax (chunk-id stx)
-    (syntax-case stx ()
-      [(_ expr (... ...))
-       (begin
-         (syntax-local-lift-expression #'(quote-syntax (a-chunk expr (... ...))))
-         (with-syntax ([((for-label-mod (... ...)) (... ...))
-                        (map (lambda (expr)
-                               (syntax-case expr (require)
-                                 [(require mod (... ...))
-                                  (let loop ([mods (syntax->list #'(mod (... ...)))])
-                                    (cond
-                                      [(null? mods) null]
-                                      [else 
-                                       (syntax-case (car mods) (for-syntax)
-                                         [(for-syntax x (... ...))
-                                          (append (loop (syntax->list #'(x (... ...))))
-                                                  (loop (cdr mods)))]
-                                         [x
-                                          (cons #'x (loop (cdr mods)))])]))]
-                                 [else null]))
-                             (syntax->list #'(expr (... ...))))])
-           #`(begin
-               (require (for-label for-label-mod (... ...) (... ...)))
-               (racketblock expr (... ...)))))])))
+(define-syntax (define-code stx)
+  (syntax-parse stx
+    [(define-code chunk-id racketblock)
+     #'(define-syntax (chunk-id stx)
+         (syntax-parse stx
+           [(_ hide:hide-opt expr (... ...))
+            (begin
+              (syntax-local-lift-expression #'(quote-syntax (a-chunk expr (... ...))))
+              (with-syntax ([((for-label-mod (... ...)) (... ...))
+                             (map (lambda (expr)
+                                    (syntax-case expr (require)
+                                      [(require mod (... ...))
+                                       (let loop ([mods (syntax->list #'(mod (... ...)))])
+                                         (cond
+                                           [(null? mods) null]
+                                           [else 
+                                            (syntax-case (car mods) (for-syntax)
+                                              [(for-syntax x (... ...))
+                                               (append (loop (syntax->list #'(x (... ...))))
+                                                       (loop (cdr mods)))]
+                                              [x
+                                               (cons #'x (loop (cdr mods)))])]))]
+                                      [else null]))
+                                  (syntax->list #'(expr (... ...))))])
+                #`(begin
+                    (require (for-label for-label-mod (... ...) (... ...)))
+                    #,(if (syntax->datum (attribute hide.hide?))
+                          #'(void)
+                          #'(racketblock expr (... ...))))))]))]))
 
 (define-code program racketblock)
 (define-code PROGRAM RACKETBLOCK)
